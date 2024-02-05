dfedbf2b619acfd3875a9c3e352568ae04e0fee4
Chaudhary Umair <umairraza95300@gmail.com>
2024-02-05 17:35:14 +0500

    got offer details button set

--- src/Components/GotOffer/GotOffer.jsx
+++ src/Components/GotOffer/GotOffer.jsx
@@ -1,5 +1,13 @@
 import React, { useEffect, useState } from "react";
-import { Modal, ModalHeader, ModalBody, ModalFooter, Button, Row, Col } from "reactstrap";
+import {
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Button,
+  Row,
+  Col,
+} from "reactstrap";
 import Slider from "react-slick";
 import { ChatState } from "../../Context/ChatProvider";
 import { GOTOFFER } from "../../Constants/Constants";
@@ -10,6 +18,22 @@ const GotOffer = ({ formattedOfferDetails, onConfirm, onCancel }) => {
   const [showModal, setShowModal] = useState(true);
   const [fullDetailsModal, setFullDetailsModal] = useState(false);
   const [imageDataURL, setImageDataURL] = useState([]);
+
+  const [showMore, setShowMore] = useState(false);
+
+  const formattedDetails = formattedOfferDetails?.details || "";
+  const truncatedDetails =
+    formattedDetails?.length > 30
+      ? formattedDetails.slice(0, 30) + "..."
+      : formattedDetails;
+
+  const displayDetails = showMore
+    ? formattedOfferDetails?.details
+    : truncatedDetails?.slice(0, 30) + "...";
+  const toggleShowMore = () => {
+    setShowMore(!showMore);
+  };
+
   let { setGotOffer } = ChatState();
 
   useEffect(() => {
@@ -35,10 +59,7 @@ const GotOffer = ({ formattedOfferDetails, onConfirm, onCancel }) => {
     document.body.style.overflow = "auto";
   };
 
-  const toggleFullDetailsModal = () => {
-    setFullDetailsModal(!fullDetailsModal);
-    document.body.style.overflow = "auto";
-  };
+
 
   const handleConfirm = () => {
     if (onConfirm) {
@@ -64,12 +85,6 @@ const GotOffer = ({ formattedOfferDetails, onConfirm, onCancel }) => {
     setGotOffer(false);
     closeModal();
   };
-
-  const formattedDetails = formattedOfferDetails?.details || "";
-  const truncatedDetails =
-    formattedDetails.length > 20
-      ? formattedDetails.slice(0, 20) + "..."
-      : formattedDetails;
 
   // const CustomPrevArrow = (props) => {
   //   const { onClick } = props;
@@ -103,18 +118,16 @@ const GotOffer = ({ formattedOfferDetails, onConfirm, onCancel }) => {
     slidesToScroll: 1,
     //  prevArrow: <CustomPrevArrow />,
     //  nextArrow: <CustomNextArrow />,
-     
-
   };
 
   return (
     <div>
-      <Modal isOpen={showModal} keyboard={false} centered >
+      <Modal isOpen={showModal} keyboard={false} centered>
         <ModalHeader>{GOTOFFER.OFFER_HEADER}</ModalHeader>
-        <ModalBody >
-
+        <ModalBody>
           <p>
-            <strong>{GOTOFFER.OFFER_TITLE}</strong> {formattedOfferDetails?.Title}
+            <strong>{GOTOFFER.OFFER_TITLE}</strong>{" "}
+            {formattedOfferDetails?.Title}
           </p>
           <p>
             <strong>{GOTOFFER.OFFER_DATE}</strong> {formattedOfferDetails?.date}
@@ -123,24 +136,40 @@ const GotOffer = ({ formattedOfferDetails, onConfirm, onCancel }) => {
             <strong>{GOTOFFER.OFFER_TIME}</strong> {formattedOfferDetails?.time}
           </p>
           <p>
-            <strong>{GOTOFFER.OFFER_AMOUNT}</strong>${formattedOfferDetails?.amount}
+            <strong>{GOTOFFER.OFFER_AMOUNT}</strong>$
+            {formattedOfferDetails?.amount}
           </p>
           <p>
-            <strong>{GOTOFFER.OFFER_SERVICE}</strong> {formattedOfferDetails?.service}
+            <strong>{GOTOFFER.OFFER_SERVICE}</strong>{" "}
+            {formattedOfferDetails?.service}
           </p>
-          <p>
-            <strong>{GOTOFFER.OFFER_DETAILS}</strong>{" "}
-            <div style={{ whiteSpace: "pre-wrap" }}>
-              {truncatedDetails.replace(/<br\s*\/?>/gi, "\n")}
+          <p style={{ maxHeight: "100px", overflowY: "scroll" }}>
+            <strong>{GOTOFFER.OFFER_DETAILS}</strong>
+            <div
+              style={{
+                whiteSpace: "pre-wrap",
+                maxHeight: showMore ? "none" : "100px",
+                overflow: "hidden",
+              }}
+            >
+              {displayDetails.replace(/<br\s*\/?>/gi, "\n")}
             </div>
+            {truncatedDetails.length > 30 && (
+              <Button
+              color="primary"
+                onClick={toggleShowMore}
+                style={{ cursor: "pointer", marginTop: "5px" }}
+              >
+                {showMore ? "Show Less" : "Show More"}
+              </Button>
+            )}
           </p>
+
           <p>
             <strong>Task Pictures</strong>
-
           </p>
-          <Row className="" >
-            
-           <Col>
+          <Row className="">
+            <Col>
               <Slider {...settings} className=" m-5">
                 {imageDataURL?.map((image, index) => (
                   <div className="" key={index}>
@@ -152,8 +181,7 @@ const GotOffer = ({ formattedOfferDetails, onConfirm, onCancel }) => {
                   </div>
                 ))}
               </Slider>
-              </Col>
-          
+            </Col>
           </Row>
         </ModalBody>
         <ModalFooter>
@@ -163,28 +191,9 @@ const GotOffer = ({ formattedOfferDetails, onConfirm, onCancel }) => {
           <Button color="danger" onClick={handleCancel}>
             {GOTOFFER.REJECT_BUTTON}
           </Button>{" "}
-          <Button color="info" onClick={toggleFullDetailsModal}>
-            {GOTOFFER.SEE_DETAILS_BUTTON}
-          </Button>
         </ModalFooter>
       </Modal>
 
-      <Modal isOpen={fullDetailsModal} toggle={toggleFullDetailsModal}>
-        <ModalHeader toggle={toggleFullDetailsModal}>{GOTOFFER.FULL_DETAILS}</ModalHeader>
-        <ModalBody style={{ maxHeight: '20vh', overflowY: 'auto' }}>
-          <p>
-            <strong>{GOTOFFER.FULL_DETAILS_HEADING}</strong>{" "}
-            <div style={{ whiteSpace: "pre-wrap" }}>
-              {formattedDetails.replace(/<br\s*\/?>/gi, "\n")}
-            </div>
-          </p>
-        </ModalBody>
-        <ModalFooter>
-          <Button color="primary" onClick={toggleFullDetailsModal}>
-            {GOTOFFER.CLOSE_BUTTON}
-          </Button>
-        </ModalFooter>
-      </Modal>
     </div>
   );
 };
